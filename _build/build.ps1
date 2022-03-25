ceb5e37263d5a7e17dae0506b42457516b29f24c
pgrho <shipwreck@live.jp>
2022-03-25 22:36:53 +0900

    Updated _config.yml

--- _build/build.ps1
+++ _build/build.ps1
@@ -7,5 +7,8 @@ New-Item "$PSScriptRoot\..\_temp" -ItemType Directory
 dotnet publish "$PSScriptRoot\..\_submodules\primagi-items\src\Shipwreck.PrimagiItems.Web\Shipwreck.PrimagiItems.Web.csproj" -c Release -o "$PSScriptRoot\..\_temp"
 Move-Item -Path "$PSScriptRoot\..\_temp\wwwroot" -Destination "$PSScriptRoot\..\primagi-items"
 
+$html = [System.IO.File]::ReadAllText("$PSScriptRoot\..\primagi-items\index.html")
+[System.IO.File]::WriteAllText("$PSScriptRoot\..\primagi-items\index.html", $html.Replace('<base href="/"', '<base href="/primagi-items/"'))
+
 $ErrorActionPreference = "SilentlyContinue"
 Remove-Item -Path "$PSScriptRoot\..\_temp" -Recurse -Force
